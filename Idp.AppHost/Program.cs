a9640b398105ee312c6617bf69d91dfd51a8525d
Damien <damien.bowden@isolutions.ch>
2024-08-30 16:22:53 +0200

    Test parameters

--- Idp.AppHost/Program.cs
+++ Idp.AppHost/Program.cs
@@ -1,6 +1,13 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+var adminUsername = builder.AddParameter("adminUsername");
+var adminPassword = builder.AddParameter("adminPassword", secret: true);
+
 var keycloak = builder.AddKeycloak("keycloak", 8081)
+    .WithEnvironment("admin", adminUsername)
+    .WithEnvironment("admin", adminPassword)
     .WithDataVolume();
 
 var apiService = builder.AddProject<Projects.Idp_ApiService>("apiservice")
