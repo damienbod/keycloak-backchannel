a19f09b796555930027ff2ee8d90bc6eebaf6d50
Damien <damien.bowden@isolutions.ch>
2024-08-30 20:10:46 +0200

    Activate the preview features

--- Idp.AppHost/Program.cs
+++ Idp.AppHost/Program.cs
@@ -6,6 +6,7 @@ var userName = builder.AddParameter("userName");
 var password = builder.AddParameter("password", secret: true);
 
 var keycloak = builder.AddKeycloakContainer("keycloak", userName: userName, password: password, port: 8081)
+    .WithArgs("--features=preview")
     .WithDataVolume();
 
 builder.AddProject<Projects.MvcHybridBackChannel>("MvcHybridBackChannel")
