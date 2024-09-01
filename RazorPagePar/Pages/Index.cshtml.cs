cf31b7aabb9a2e0139adeed714e76b0da7e69ef0
Damien <damien.bowden@isolutions.ch>
2024-09-01 15:16:02 +0200

    Add RazorPagePar

---
+++ RazorPagePar/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagePar.Pages;
+
+[Authorize]
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ RazorPagePar/Pages/Logout.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagePar.Pages;
+
+[Authorize]
+public class LogoutModel : PageModel
+{
+    public IActionResult OnGet()
+    {
+        return SignOut(
+            new AuthenticationProperties { RedirectUri = "/SignedOut" },
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            OpenIdConnectDefaults.AuthenticationScheme);
+    }
+}
