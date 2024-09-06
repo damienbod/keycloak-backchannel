932de8cc561ca91000aa03d457ae8361843ac625
Damien <BowdenD@ne.ch>
2024-09-06 16:25:04 +0200

    clean up logs

--- DPoPApi/Program.cs
+++ DPoPApi/Program.cs
@@ -13,7 +13,7 @@ try
 
     builder.Host.UseSerilog((ctx, lc) => lc
         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
-        .WriteTo.File("../_logs-api.txt")
+        .WriteTo.File("../_logs-dpop-api.txt")
         .Enrich.FromLogContext()
         .ReadFrom.Configuration(ctx.Configuration));
 
