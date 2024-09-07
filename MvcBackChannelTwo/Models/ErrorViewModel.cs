18073f1f129e5d616fa89f8e8b98a12a29dada72
damienbod <damien_bod@hotmail.com>
2024-09-07 10:18:43 +0200

    Update UI

---
+++ MvcBackChannelTwo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcBackChannelTwo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
